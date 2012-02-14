f8248d29c75c726327a513a938a93840267fdccf
Matt LaMond <matt@roflinc.com>
2012-02-13 20:10:28 -0500

    Initial Commit

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by Matthew LaMond on 2/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
